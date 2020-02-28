69977bf71c02d32b3d078c0103f490c096c8fabf
Rafael Alonso <rafael.palonso@gmail.com>
2020-02-28 10:34:56 -0300

    Going further with geocoder and mapbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,8 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from 'plugins/init_mapbox';
+import { initAutocomplete } from 'plugins/init_place';
 
 initMapbox();
+initAutocomplete();
